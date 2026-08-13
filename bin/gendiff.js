c66af31925a87cfcb9a53115ede7f1b074e7e9bf
Вячеслав Лебедев <vaceslavlebedev825@gmail.com>
2026-08-13 17:22:09 +0500

    fix: update CLI version and enforce required format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ const program = new Command();
 program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
-  .version("0.0.1", "-V, --version", "output the version number")
+  .version("1.0.0", "-V, --version", "output the version number")
   .arguments("<filepath1> <filepath2>")
-  .option("-f, --format [type]", "output format", "stylish") // Установили дефолт stylish
+  .option("-f, --format <type>", "output format", "stylish") // Установили дефолт stylish
   .helpOption("-h, --help", "display help for command")
   .action((filepath1, filepath2, options) => {
     const result = genDiff(filepath1, filepath2, options.format);
